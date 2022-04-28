2d648be8878f6bce3ac3a84e828dae035f48d5cd
Marcelo Gachet <marcelo@growdnd.com>
2022-04-28 16:28:47 -0700

    Partial fix for editing posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-router.get('/', (req, res) => {
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
   Post.findAll({
     where: {
       // use the ID from the session
